4459abda1cb778e60c151e00cf675086a7f74843
mrimal <mrimal@worldbank.org>
2017-04-07 18:52:18 -0400

    scraped tables by year
    burn rate included

--- fyChange.py
+++ fyChange.py
@@ -34,9 +34,30 @@ for projectID in projectIDs:
     for option in year_list.find_all('option'):
         indi_year = option.text
         print(indi_year)
-    
-    
         
+        response = webdriver.request('POST', "http://operationsportal.worldbank.org/dbbudgetExpenseSummary", data={"fiscalYear": indi_year, "projectId":projectID})
+        try:
+            table = soup.find('table', {"class": "table summary-table"})
+            rows = table.find_all('tr')
+        except AttributeError as e:
+            print 'No table found'
+            
+        for row in rows :
+            table_headers = row.find_all('th')
+            if table_headers:
+               finalTable.append([headers.get_text() for headers in table_headers])
+               
+            table_data = row.find_all('td', attrs={})    
+            if table_data:
+                finalTable.append([data.get_text() for data in table_data])
+
+budgetTable = pandas.DataFrame(finalTable, index=None)
+
+print(budgetTable)
+
+writer = pandas.ExcelWriter('newOutput.xlsx')
+budgetTable.to_excel(writer, 'Sheet1' )
+writer.save()  
         
         
         
