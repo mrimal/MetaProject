8c89a6d825668da5afadf5f11c6b4db40a6a3da3
mrimal <mrimal@worldbank.org>
2017-04-07 17:31:39 -0400

    Plugging along

--- cTestRequests.py
+++ cTestRequests.py
@@ -13,35 +13,38 @@ import pandas
 from pandas import DataFrame
 
 webdriver = Chrome()
-response = webdriver.request('POST', config.postUrl, data={"opspage": "dashboard > Budget", "disbRatio":"0.0", "projectId":config.projectID, "fiscalYear":"", "asaFlag":"false"})
-html2 = response.text
-encodedHtml = html2.encode('ascii', 'ignore')
+
 #text_file = open("Output.txt", "w")
 #text_file.write("%s" % encodedHtml)
 #text_file.close()
 
-soup = BeautifulSoup(html2)
+
 projectIDs = pandas.read_csv('projects.csv')
 
 finalTable= []
 for projectID in projectIDs:
+    
+    response = webdriver.request('POST', config.postUrl, data={"opspage": "dashboard > Budget", "disbRatio":"0.0", "projectId":projectID, "fiscalYear":"", "asaFlag":"false"})
+    html2 = response.text
+    encodedHtml = html2.encode('ascii', 'ignore')
+    soup = BeautifulSoup(html2)
     
     try:
         table = soup.find('table', {"class": config.budgetTableClass})
         rows = table.find_all('tr')
     except AttributeError as e:
         print 'No table found'
-        
+    
+   
     for row in rows :
             table_headers = row.find_all('th')
             if table_headers:
-                finalTable.append([headers.get_text() for headers in table_headers])
-                #finalTable.insert(0,config.projectID)
-            
+               finalTable.append([headers.get_text() for headers in table_headers])
+               
             table_data = row.find_all('td', attrs={})    
             if table_data:
                 finalTable.append([data.get_text() for data in table_data])
-                #finalTable.insert(0,config.projectID)
+
 
 budgetTable = pandas.DataFrame(finalTable, index=None)
 
